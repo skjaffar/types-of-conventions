4a91ea3248e6d8cb6456485bd33a852bb77c4171
skjaffar <107207838+skjaffar@users.noreply.github.com>
2022-06-18 17:29:12 +0530

    Add files via upload

---
+++ type of conversion.py
@@ -0,0 +1,42 @@
+# a=9
+# print(type(a))
+# print(a,type(a))
+# print(float(a))
+# print(str(a))
+
+
+# z=18
+# p='jaffar'
+# print(p+str(z))
+# print(p+'   '+str(z))
+# # print(p+z)
+# print(p+'  '+'version 3.10.2')
+
+# c=9.2
+# print(c,type(c))
+# print(int(c))
+# print(str(c))
+
+# m='5'
+# print(m,type(m))
+# print(int(m)) 
+# print(float(m)) 
+
+''' important '''
+
+# print('ha' + 'i')
+# print('monday'+'  '+ str(18)+'thmay')
+
+
+
+
+# m='jaFaR'
+# print(m.swapcase())
+# print(m.isupper())
+# print(m.islower())
+
+# k='jan'
+# print(k.islower())
+
+# b='JAFFAR'
+# print(b.isupper())
